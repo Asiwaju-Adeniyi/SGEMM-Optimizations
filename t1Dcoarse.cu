48dd244d38a09f4bdd41e7dc1bc41201bbce1f37
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-02-28 00:29:17 +0100

    Create t1Dcoarse.cu

---
+++ t1Dcoarse.cu
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+
+#define CEIL_DIV(M, N) (((M) + (N) - 1) / (N))
+
+
+__global__ void tCoarsen_kernel(int M, int N, int K, const float* A, const float* B, float* C, float alpha, float beta) {
+    
+const int BM = 64;
+const int BN = 64;
+const int BK = 8;                                                                                                                                                                                                                                                                                                                                                                       
+const int TM = 8;
+
+     const uint cRow = threadIdx.y;
+     const uint cCol = threadIdx.x;
+
+     const uint totalBTresults = BM * BN;
+     const uint numThreadsPerBlock = totalBTresults / TM;
+
+     assert(numThreadsPerBlock == blockDim.x);
+
+     const int tRow = threadIdx.x / BK;
+     const int tCol = threadIdx.x % BK;
+
+     __shared__ float As[BM * BK];
+     __shared__ float Bs[BM * BK];
+
+     A += cRow * BK * K;
+     B += cCol * BK;
+     C += cRow * BM * N + cCol * BN;
+
+
+    const uint inColA = threadIdx.x % BK;
+    const uint inRowA = threadIdx.x / BK; 
+     
+    const uint inRowB = threadIdx.x / BK;
+    const uint inColB = threadIdx.x % BK;
